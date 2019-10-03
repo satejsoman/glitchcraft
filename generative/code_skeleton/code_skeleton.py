1d2a599e34843cb4be735cc3537e93ac4d05546e
satej soman <satej.soman@gmail.com>
2019-10-03 01:56:39 -0500

    Update code_skeleton.py

--- generative/code_skeleton/code_skeleton.py
+++ generative/code_skeleton/code_skeleton.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python
 
 #handy imports  
+import re
 from keyword import iskeyword
 
 #parameters
 w, h = 900, 1500
-code_start = 24
+code_start = 30
 code_size = 7 #code line thickness
 code_lines = 70
 code_sep = 15
-indent_size = 32
-letter_spacing, word_spacing = 6, 12
+indent_size = 45
+letter_spacing, word_spacing = 5, 10
 
 #define colors
 bg_color = (25, 25, 25)
 palette = {
-    "string": (229, 115, 118), 
-    "keyword": (235, 167, 114),
     "text": (120, 120, 120),
+    "string": (229, 115, 118),
     "comment": (60, 60, 60), 
+    "keyword": (235, 167, 114),
     "builtin": (116, 178, 208),
-    "symbols": (180, 180, 180)   
+    "symbols": (180, 190, 180)   
 }
 
-QUOTE = chr(34)
+QUOTE, STAR, HASH, BANG, DOT = list(map(chr, [34, 42, 35, 33, 46]))
+
+filename, filetype = "skeleton_quine", "png"
 
 def set_color(rgb):
     stroke(*rgb)
@@ -34,10 +37,8 @@ def setup():
     
     with open(__file__, "r") as f:
         content = f.readlines()
-        num_lines = len(content) 
     
     #size and background
-    h = (num_lines + 2) * code_sep/2
     size(w, h)
     background(*bg_color)
     
@@ -50,12 +51,12 @@ def setup():
 
     line_y = code_start
     for code_line in content:
-        comment_mode = code_line[0] == "#"
+        comment_mode = code_line[0] == HASH
         num_spaces = 0
         while code_line[num_spaces] == " " and num_spaces < len(code_line) - 1:
             num_spaces += 1
         line_x = indent_size  + num_spaces * letter_spacing
-        for word in code_line.split():
+        for word in re.findall(r"[\"?\w\"?']+|[\x20-\x7E]", code_line.strip()):
             keyword_mode = False
             builtin_mode = False
             if iskeyword(word) and not comment_mode:
@@ -70,24 +71,27 @@ def setup():
                 set_color(palette["text"])
             string_mode = False
             for c in word:
-                if c == QUOTE and not comment_mode:
-                    string_mode = not string_mode
-                    if string_mode:
-                        set_color(palette["string"])
-                elif c == "#" and not string_mode:
-                    comment_mode = True
-                    set_color(palette["comment"])
-                if c.isalnum() or c in "_'":
-                    if not any([keyword_mode, comment_mode, builtin_mode, string_mode]):
-                        set_color(palette["text"])
-                    line(line_x, line_y, line_x + letter_spacing, line_y)
-                    line_x += letter_spacing
+                if  c == " ":
+                    line_x +=  word_spacing
                 else: 
-                    offset = -2 if (line_x == indent_size or c in "*#!") else 4
-                    if not any([keyword_mode, comment_mode, builtin_mode, string_mode, c == QUOTE]):
-                        set_color(palette["symbols"])
-                    text(c, line_x + offset, line_y + 0.3*code_sep)
-                    line_x += 15
-            line_x += word_spacing
+                    if c == QUOTE and not comment_mode:
+                        string_mode = not string_mode
+                        if string_mode:
+                            set_color(palette["string"])
+                    elif c == HASH and not string_mode:
+                        comment_mode = True
+                        set_color(palette["comment"])
+                    if c.isalnum() or c in "_'":
+                        if not any([keyword_mode, comment_mode, builtin_mode, string_mode]):
+                            set_color(palette["text"])
+                        line(line_x, line_y, line_x + letter_spacing, line_y)
+                        line_x += letter_spacing
+                    else: 
+                        offset = -4 if (line_x == indent_size or c in [STAR, HASH, BANG]) else 4
+                        if not any([keyword_mode, comment_mode, builtin_mode, string_mode, c == QUOTE]):
+                            set_color(palette["symbols"])
+                        text(c, line_x + offset, line_y + 0.3*code_sep)
+                        line_x += letter_spacing + (word_spacing if c == QUOTE or c in "([\\?./" else letter_spacing)
+ 
         line_y += code_sep 
-    save("skeleton_quine.png")
+    save(DOT.join([filename, filetype]))
